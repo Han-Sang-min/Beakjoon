6caf121fff738d9e1fedc5282a2f05813d4000a4
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-04-14 12:26:32 +0900

    [Bronze IV] Title: 시험 성적, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,27 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
-		BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
 		
 		int score = Integer.valueOf(bw.readLine());
 		
 		if (score >= 90)
-			br.write("A");
+			System.out.println("A");
 		else if (score >= 80)
-			br.write("B");
+			System.out.println("B");
 		else if (score >= 70)
-			br.write("C");
+			System.out.println("C");
 		else if (score >= 60)
-			br.write("D");
+			System.out.println("D");
 		else
-			br.write("F");
-		br.flush();
-		br.close();
+			System.out.println("F");
 	}
 }
