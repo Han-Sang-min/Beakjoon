45612b13d8646a3a594845ef0b33b9845401a395
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-04-18 19:48:03 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j + i < (n-1)*2 + 1; j++)
+		{
+			if (i <= j && (n-1)*2 >= j + i) cout << "*";
+			else cout << " ";
+		}
+		cout << '\n';
+	}
+}
