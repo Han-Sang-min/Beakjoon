d06bdd9707fffda0a12b22eddf49e93c7e5db25a
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-05-04 01:19:16 +0900

    [Silver II] Title: 종이의 개수, Time: 348 ms, Memory: 20928 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -0,0 +1,44 @@
+	#include <bits/stdc++.h>
+	using namespace std;
+
+	int n;
+	int cnt[3];
+	int pap[2200][2200];
+
+	bool is_ok(int a, int b, int n)
+	{
+		for (int i = a; i < n + a; i++)
+			for (int j = b; j < n + b; j++)
+				if (pap[a][b] != pap[i][j]) return false;
+		return true;
+	}
+	/*
+	for (int i = a; i < n + a - 1; i++)
+		for (int j = b; j < n + b - 1; j++)
+			if (pap[i][j] != pap[i + 1][j + 1]) return false;
+	*/
+	void proc(int a, int b, int n)
+	{
+		if (is_ok(a, b, n))
+		{
+			cnt[pap[a][b] + 1]++;
+			return;
+		}
+		int tn = n / 3;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				proc(a + i * tn, b + j * tn, tn);
+	}
+
+	int main(void) {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+	
+		cin >> n;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				cin >> pap[i][j];
+		proc(0, 0, n);
+		for (int i = 0; i < 3; i++)
+			cout << cnt[i] << "\n";
+	}
