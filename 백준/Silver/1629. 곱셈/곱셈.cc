3c308dca496670df6dfdac27ae95c1a078b7d4f0
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-05-03 15:48:05 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -2,19 +2,19 @@
 using namespace std;
 
 using ll = long long;
-ll proc(ll a, ll b, ll c)
+ll pow(ll a, ll b, ll c)
 {
 	if (b == 1) return a % c;
-	ll val = proc(a, b / 2, c);
-	val = val * val % c;
-	if (b % 2 == 0) return val;
-	return val * a % c;
+	ll rem = pow(a, b / 2, c);
+	if (b % 2) return rem * rem % c * a % c;
+	return rem * rem % c;
 }
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
+	
 	ll a, b, c;
 	cin >> a >> b >> c;
-	cout << proc(a, b, c);
+	cout << pow(a, b, c);
 }
