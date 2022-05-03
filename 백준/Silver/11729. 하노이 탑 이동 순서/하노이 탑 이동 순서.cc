3cfcf367deb494d900c415fc791b9cad0e167c7b
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-05-03 16:20:03 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,24 @@
+	#include <bits/stdc++.h>
+	using namespace std;
+
+	void proc(int a, int b, int n)
+	{
+		if (n == 1)
+		{
+			cout << a << " " << b << "\n";
+			return;
+		}
+		proc(a, 6 - a - b, n - 1);
+		cout << a << " " << b << "\n";
+		proc(6 - a - b, b, n - 1);
+	}
+
+	int main(void) {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+	
+		int n;
+		cin >> n;
+		cout << (1 << n) - 1 << '\n';
+		proc(1, 3, n);
+	}
