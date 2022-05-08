66974eaccc3dd1aecdc1763ae41a8f59ff9691da
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-05-08 11:31:15 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, ck_nu;
+int pnt[10], arr[10];
+
+void solve(int k)
+{
+    if (k == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << pnt[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (ck_nu <= arr[i])
+        {
+            ck_nu = arr[i];
+            pnt[k] = arr[i];
+            solve(k + 1);
+            ck_nu = 0;
+        }
+    }
+}
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    solve(0);
+}
