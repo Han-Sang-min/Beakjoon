9fed42d934670fff26569ee446b09b4acfc61dd0
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-06-21 22:17:58 +0900

    [Gold II] Title: 2048 (Easy), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -1,28 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[25][25];
-int t_board[25][25];
-int n, tmp, res;
-vector <pair <int, int>> emty_pos;
-
-void pnt()
-{
-	cout << "\n";
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			cout << t_board[i][j] << " ";
-		cout << "\n";
-	}
-}
-
-int pow(int nu, int power)
-{
-	if (power == 0)
-		return 1;
-	return nu * pow(nu, power - 1);
-}
+int n;
+int res;
+int board[21][21];
+int t_board[21][21];
 
 void init_t_board()
 {
@@ -31,20 +13,9 @@ void init_t_board()
 			t_board[i][j] = board[i][j];
 }
 
-int find_big()
+void rote_board()
 {
-	int ret = 0;
-
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			if (ret < t_board[i][j])
-				ret = t_board[i][j];
-	return ret;
-}
-
-void rot_board()
-{
-	int tmp[25][25];
+	int tmp[21][21];
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
@@ -54,86 +25,35 @@ void rot_board()
 			t_board[i][j] = tmp[j][n - 1 - i];
 }
 
-void move_board2()
+void move_board(int dir)
 {
+	while (dir--) rote_board();
 	for (int i = 0; i < n; i++)
 	{
+		int tmp[21] = {};
+		int pos = 0;
 		for (int j = 0; j < n; j++)
 		{
-			if (t_board[i][j] == 0)
-				continue;
-			for (int k = j + 1; k < n; k++)
-			{
-				if (t_board[i][k] == 0)
-					continue;
-				if (t_board[i][j] == t_board[i][k])
-				{
-					t_board[i][j] *= 2;
-					t_board[i][k] = 0;
-					j = k;
-					break;
-				}
-				else
-					break;
-			}
+			if (t_board[i][j] == 0) continue;
+			else if (tmp[pos] == 0)
+				tmp[pos] = t_board[i][j];
+			else if (tmp[pos] == t_board[i][j])
+				tmp[pos++] *= 2;
+			else
+				tmp[++pos] = t_board[i][j];
 		}
-	}
-	for (int i = 0; i < n; i++)
-	{
-		emty_pos.clear();
 		for (int j = 0; j < n; j++)
-		{
-			if (t_board[i][j] == 0)
-				emty_pos.push_back({ i, j });
-			else if (!emty_pos.empty())
-			{
-				t_board[emty_pos[0].first][emty_pos[0].second] = t_board[i][j];
-				t_board[i][j] = 0;
-				emty_pos.erase(emty_pos.begin());
-				j--;
-			}
-		}
+			t_board[i][j] = tmp[j];
 	}
 }
 
-void move_board1(int dir)
+int find_big()
 {
-	if (dir == 0)
-		move_board2();
-	else if (dir == 1)
-	{
-		rot_board();
-		move_board2();
-		rot_board();
-		rot_board();
-		rot_board();
-	}
-	else if (dir == 2)
-	{
-		rot_board();
-		rot_board();
-		move_board2();
-		rot_board();
-		rot_board();
-	}
-	else if (dir == 3)
-	{
-		rot_board();
-		rot_board();
-		rot_board();
-		move_board2();
-		rot_board();
-	}
-}
-
-int proc_dir(int dir)
-{
-	for (int i = 0; i < 5; i++)
-	{
-		move_board1(dir % 4);
-		dir /= 4;
-	}
-	return find_big();
+	int ret = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			ret = max(ret, t_board[i][j]);
+	return ret;
 }
 
 int main()
@@ -145,15 +65,16 @@ int main()
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			cin >> board[i][j];
-	for (int i = 0; i < pow(4, 5); i++)
+	for (int i = 0; i < 1024; i++)
 	{
 		init_t_board();
-		tmp = proc_dir(i);
-		if (res < tmp)
+		int dir = i;
+		for (int j = 0; j < 5; j++)
 		{
-			//pnt();
-			res = tmp;
+			move_board(dir % 4);
+			dir /= 4;
 		}
+		res = max(res, find_big());
 	}
 	cout << res << "\n";
 }
