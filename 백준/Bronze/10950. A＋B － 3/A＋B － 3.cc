389858af3ebff5899d28cb74900e1ae60a48bbfd
Han-Sang-min <61174337+Han-Sang-min@users.noreply.github.com>
2022-06-30 19:54:57 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int res, n;
+
+int main(void) 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+ }
